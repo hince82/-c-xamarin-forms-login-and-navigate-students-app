9e04ff60d9f619dd10f9d1c6fc32faae9407f484
halit <41735411+hince82@users.noreply.github.com>
2018-08-13 04:13:46 +0300

    Add files via upload

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,1 +1,30 @@
+﻿using Xamarin.Forms.Xaml;
+using Xamarin;
+using Xamarin.Forms;
+using Xamarin.Android;
+using Xamarin.Android.Net;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Internals;
+using Android.App;
+
+using Android.Content.PM;
+
+using Android.OS;
+
+
+namespace LoginNavigation.Droid
+{
+	[Activity (Label = "LoginNavigation.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+            LoadApplication (new LoginNavigation.App());
+		}
+	}
+}
 
---
+++ Droid/SQLiteService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms.Xaml;
+using Xamarin;
+using Xamarin.Forms;
+using Xamarin.Android;
+using Xamarin.Android.Net;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Internals;
+using LoginNavigation.Droid;
+using System.IO;
+
+using SQLite.Net;
+
+[assembly: Dependency(typeof(SqliteService))]
+namespace LoginNavigation.Droid
+{
+    public class SqliteService : ISQLite
+    {
+        public SqliteService() { }
+
+        #region ISQLite implementation
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "/Data/Students.db";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+            var path = Path.Combine(documentsPath, sqliteFilename);
+            Console.WriteLine(path);
+            if (!File.Exists(path)) File.Create(path);
+            var plat = new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid();
+            var conn = new SQLiteConnection(plat, path);
+            // Return the database connection 
+            return conn;
+        }
+
+        #endregion
+
+
+    }
+}
